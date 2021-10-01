d6dc3a2ee3c44b8e10ad607cfeba57d4a5953179
Clément <c.spileers@outlook.fr>
2021-10-01 21:17:39 +0200

    added newsletter page

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const dessinsCard = document.querySelector("#dessins");
 const audioProjectCard = document.querySelector("#audio-project");
+const newsletterCard = document.querySelector("#newsletter");
 
-audioProjectCard.addEventListener("click", () => {document.location.href = `./pages/audioProject.html`})
-
-dessinsCard.addEventListener("click", () => {
-document.location.href = `./pages/dessins.html`;
-});
+audioProjectCard.addEventListener("click", () => {document.location.href = `./pages/audioProject.html`});
+//dessinsCard.addEventListener("click", () => {document.location.href = `./pages/dessins.html`});
+newsletterCard.addEventListener("click", () => {document.location.href = `./pages/newsletter.html`;});
