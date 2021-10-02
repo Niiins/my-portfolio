681f12609eccd7440d5670f3ec21e2e80b7a9e5c
Clément <c.spileers@outlook.fr>
2021-10-02 16:32:43 +0200

    added more projects

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const dessinsCard = document.querySelector("#dessins");
 const audioProjectCard = document.querySelector("#audio-project");
 const newsletterCard = document.querySelector("#newsletter");
+const moiPresidentCard = document.querySelector("#moi-president");
+const darkKnightCard = document.querySelector("#dark-knight");
+const bedNightCard = document.querySelector("#bed-night");
 
 audioProjectCard.addEventListener("click", () => {document.location.href = `./pages/audioProject.html`});
 //dessinsCard.addEventListener("click", () => {document.location.href = `./pages/dessins.html`});
-newsletterCard.addEventListener("click", () => {document.location.href = `./pages/newsletter.html`;});
+newsletterCard.addEventListener("click", () => {document.location.href = `./pages/newsletter.html`});
+moiPresidentCard.addEventListener("click", () => {document.location.href = `./pages/moiPresident.html`});
+darkKnightCard.addEventListener("click", () => {document.location.href = `./pages/darkKnight.html`});
+bedNightCard.addEventListener("click", () => {document.location.href = `./pages/bedNight.html`});
+
+
